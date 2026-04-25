c5476fa322f0d7dbf80b73a9d2ce2321c9b751e9
Ahmad Bilal Fazal <ahmadbilalfazal7@gmail.com>
2026-04-26 03:17:40 +0500

    Fix uploaded media file serving

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404, handler500
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
 
-handler404 = 'core.views.custom_404'
-handler500 = 'core.views.custom_500'
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
